e0a9092bc8f29c03f38af9ef5aaf7a1dc8a46a13
unknown <trahy032@gmail.com>
2023-06-30 16:42:59 +0930

    added functions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 // Assignment code here
 
-var pwdLength = 0;
+// Query Selectors
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+// var pwdLength = 0;
 var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numbers = '0123456789';
@@ -18,38 +22,52 @@ console.log(pwdUppercase)
 console.log(pwdNumbers)
 console.log(pwdSpecial)
 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 // creating random function
-function getRandomLower(){
-  return Math.floor(Math.random()*pwdLowercase.length);
+function getRandomLower() {
+  return Math.floor(Math.random() * pwdLowercase.length);
 }
 
-function getRandomUpper(){
-  return Math.floor(Math.random()*pwdUppercase.length);
+function getRandomUpper() {
+  return Math.floor(Math.random() * pwdUppercase.length);
 }
 
-function getRandomNumber(){
-  return Math.floor(Math.random()*pwdNumbers.length);
+function getRandomNumber() {
+  return Math.floor(Math.random() * pwdNumbers.length);
 }
 
-function getRandomSpecial(){
-  return Math.floor(Math.random()*pwdSpecial.length);
+function getRandomSpecial() {
+  return Math.floor(Math.random() * pwdSpecial.length);
 }
 
 
+// generate password function
+function generatePassword() {
 
+  // choosing password criteria
+  var lowerLet = window.confirm("Do you want your password to contain lowercase letters?");
+  var upperLet = window.confirm("Do you want your password to contain uppercase letters?");
+  var numLet = window.confirm("Do you want your password to contain numbers?");
+  var specialLet = window.confirm("Do you want your password to contain special characters?");
+  var pwdLength = prompt("Enter a password length of at least 8 characters and no more than 128 characters.");
 
+  // creating rule to only accept 8-128 characters
+  if (pwdLength < 8 || pwdLength > 128) {
+    window.alert("Your password length is not between 8 and 128 characters. Please try again.");
+    pwdLength;
+  }
+}
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
