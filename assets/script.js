59525691ec73bcaec457589893de0d7a42d09e0d
unknown <trahy032@gmail.com>
2023-07-02 20:06:34 +0930

    updated major function

--- assets/script.js
+++ assets/script.js
@@ -9,59 +9,46 @@ var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numbers = '0123456789';
 var special = '!@#$%^&*()_+-=,./<>?[]{}`~';
-
-// converting strings into array
-pwdLowercase = lowercase.split('')
-pwdUppercase = uppercase.split('')
-pwdNumbers = numbers.split('')
-pwdSpecial = special.split('')
-
-// see array length
-console.log(pwdLowercase)
-console.log(pwdUppercase)
-console.log(pwdNumbers)
-console.log(pwdSpecial)
-
+var charSet = "";
+var password = "";
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-// creating random function
-function getRandomLower() {
-  return Math.floor(Math.random() * pwdLowercase.length);
-}
-
-function getRandomUpper() {
-  return Math.floor(Math.random() * pwdUppercase.length);
-}
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * pwdNumbers.length);
-}
-
-function getRandomSpecial() {
-  return Math.floor(Math.random() * pwdSpecial.length);
-}
-
 
 // generate password function
 function generatePassword() {
 
   // choosing password criteria
-  var lowerLet = window.confirm("Do you want your password to contain lowercase letters?");
-  var upperLet = window.confirm("Do you want your password to contain uppercase letters?");
-  var numLet = window.confirm("Do you want your password to contain numbers?");
-  var specialLet = window.confirm("Do you want your password to contain special characters?");
   var pwdLength = prompt("Enter a password length of at least 8 characters and no more than 128 characters.");
 
   // creating rule to only accept 8-128 characters
   if (pwdLength < 8 || pwdLength > 128) {
-    window.alert("Your password length is not between 8 and 128 characters. Please try again.");
-    pwdLength;
+    alert("Your password length is not between 8 and 128 characters. Please try again.");
+    return;
   }
-}
+  var lowerSet = confirm("Do you want your password to contain lowercase letters?");
+  var upperSet = confirm("Do you want your password to contain uppercase letters?");
+  var numSet = confirm("Do you want your password to contain numbers?");
+  var specialSet = confirm("Do you want your password to contain special characters?");
 
+  // using criteria to generate password
+  if (lowerSet) {
+    charSet += lowercase;
+  }
+  if (upperSet) {
+    charSet += uppercase;
+  }
+  if (numSet) {
+    charSet += numbers;
+  }
+  if (specialSet) {
+    charSet += special;
+  }
+
+  for (i = 0; i < pwdLength; i++) {
+    password += charSet[Math.floor(Math.random() * charSet.length)]
+  } return password;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -69,5 +56,4 @@ function writePassword() {
 
 
   passwordText.value = password;
-
 }
